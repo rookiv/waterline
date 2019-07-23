179df1aca8f375fd062ad9537f98bf8ec244b288
rukqoa <rukqoa@gmail.com>
2019-07-22 22:47:05 -0700

    Add various sessions and instance handling

--- app.js
+++ app.js
@@ -6,15 +6,19 @@ const express = require('express');
 const url = require('url');
 const app = express();
 
+// Post handling
+app.use(express.urlencoded());
+app.use(express.json());
+
 const config = require('./config.js');
 
 // Get canned responses
 const responseFile = require('./responses.json');
 let responses = {
-    "POST": [],
-    "GET": [],
-    "PUT": [],
-    "PATCH": []
+    'POST': [],
+    'GET': [],
+    'PUT': [],
+    'PATCH': []
 };
 responseFile.forEach((entry) => {
     Object.keys(responses).forEach((requestType) => {
@@ -25,13 +29,147 @@ responseFile.forEach((entry) => {
     });
 });
 
+let instances = {};
+let sessions = {};
+
+/* Routes */
+
+//Welcome
+app.get('/', (req, res) => {
+    const {version} = require('./package.json');
+    res.send('Waterline version: ' + version);
+});
+
+// Poll
+app.get('/presence/connect', (req, res) => {
+    let message = '["welcome",{"success":true,"data":{"connection_id":"180182b9-ee86-4442-8ba5-2df0aad31e20","connected_at":"Sun, 31 Mar 2019 13:06:12 GMT"}}]\\n';
+    res.writeHead(200, {'Content-Type': 'application/x-json-stream'});
+    res.write(message);
+
+    let iteration = 300;
+    let poll = setInterval(() => {
+        iteration--;
+        if (iteration <= 0) {
+            res.end();
+            clearInterval(poll);
+        } else {
+            res.write('\n');
+        }
+    }, 1000);
+});
+
+// Create instance and add metadata
+app.get('/o/_instances/:instanceId/metadata', (req, res) => {
+    let displayId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    let instanceId = req.params.instanceId;
+
+    instances[instanceId] = {
+        'name': displayId
+    };
+
+    let response = {
+        'attributes': {},
+        'display_name': displayId,
+        'owner': '25721053-ebe8-59f1-95b3-de39bc253c7f'
+    };
+
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(response);
+    res.end();
+});
+
+// Associate instance with session
+app.post('/o/_instances/:instanceId/associate_with_session', (req, res) => {
+    let sessionId = req.body.session_id;
+    let instanceId = req.params.instanceId;
+
+    instances[instanceId].sessionId = sessionId;
+
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write({});
+    res.end();
+});
+
+// Create session
+app.post('/o/_sessions/create', (req, res) => {
+    let sessionId = req.body.session_id;
+    let address = req.body.join_params.serverAddress;
+    let port = req.body.join_params.serverPort;
+    let joinChallengeKey = req.body.join_params.joinChallengeKey;
+    let matchChallengeKey = req.body.join_params.matchChallengeKey;
+    let serverProfile = req.body.attributes.serverProfile;
+    let state = req.body.attributes.state;
+    let name = req.body.name;
+    let region = req.body.region;
+    let slotCount = req.body.slot_count;
+    let disableJoining = req.body.attributes.disableJoining;
+    let gameMode = req.body.attributes.gameMode;
+    let isPublic = req.body.attributes.public;
+    let timeString = (new Date()).toUTCString();
+
+    sessions[sessionId] = {
+        'address': address,
+        'port': port,
+        'joinChallengeKey': joinChallengeKey,
+        'matchChallengeKey': matchChallengeKey,
+        'serverProfile': serverProfile,
+        'state': state,
+        'name': name,
+        'region': region,
+        'slotCount': slotCount,
+        'disableJoining': disableJoining,
+        'gameMode': gameMode,
+        'isPublic': isPublic,
+        'timeString': timeString
+    };
+
+    let response = {
+        "_created_at": timeString,
+        "_id": sessionId,
+        "_modified_at": timeString,
+        "_type": "_sessions",
+        "attributes": {
+            "disableJoining": disableJoining,
+            "gameMode": gameMode,
+            "ip": address,
+            "public": isPublic,
+            "serverProfile": serverProfile,
+            "state": state
+        },
+        "filled_slots": 0,
+        "host": "25721053-ebe8-59f1-95b3-de39bc253c7f",
+        "last_keepalive": timeString,
+        "name": name,
+        "password_protected": false,
+        "purpose": isPublic ? "public" : "private",
+        "region": region,
+        "reservation_count": 0,
+        "slot_count": slotCount,
+        "state": "created"
+    };
+
+    res.writeHead(201, {'Content-Type': 'application/json', 'Content-Location': '/o/_sessions/' + sessionId});
+    res.write(response);
+    res.end();
+});
+
+// Update session
+app.patch('/o/_sessions/:sessionId/metadata', (req, res) => {
+});
+
+// Keep session alive
+app.post('/o/_sessions/:sessionId/touch', (req, res) => {
+});
+
+
+// Canned responses
 app.all('*', (req, res) => {
     let requestType = req.method;
     let cannedResponse = responses[requestType][req.url];
 
     if (!cannedResponse) {
         console.log(req.url)
-        res.status(404).send("Not found");
+        res.status(404).send('Not found');
         return;
     }
 
